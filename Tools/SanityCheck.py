c1f9f1d1f407674815fbec926fb24a70d5bc4788
Nicolas <wiest.daessle@gmail.com>
2020-08-24 09:55:12 +0200

    Update to script & CSV Checking file
    Versionning update
    Script for checking sanity of CSV file !

--- Tools/SanityCheck.py
+++ Tools/SanityCheck.py
@@ -7,6 +7,15 @@ This is a temporary script file.
 
 #%%
 
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk)) 
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk)) 
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk)) 
+
 core=set()
 maped=dict()
 started=0
@@ -35,7 +44,10 @@ f.close()
 print("Number of started processed    :",started)
 print("Remaining accepted processes   :", len(maped))
 print("Remaining unfinished processes :", len(core))
-
+if (len(maped) > 0 or len(core) > 0):
+    prRed("KO")
+else:
+    prGreen("OK")
 
 
 #%%
