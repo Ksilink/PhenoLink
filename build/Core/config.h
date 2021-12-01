39e7810970dcbcfbedfff95a7d859236777faf49
Nicolas <wiest.daessle@gmail.com>
2021-12-01 16:26:28 +0100

    Revert "not sure of that one"
    This reverts commit 3e1b15cae38fcd69b21e59c4ba6157acfd583d98.

--- build/Core/config.h
+++
@@ -1,9 +0,0 @@
-
-
-#define Checkout_VERSION_MAJOR 2
-#define Checkout_VERSION_MINOR 0
-#define Checkout_VERSION_COMPLEMENT R
-
-#define Checkout_VERSION_RELEASE 3-edf0b5b-1f20f28
-
-#define CHECKOUT_VERSION "2.0.R3-edf0b5b-1f20f28"
--- build/Testing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Checkout_2020/CheckoutApp/Testing
-# Build directory: D:/Checkout_2020/CheckoutApp/build/Testing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
