1df6cb41659bc9f05e17951594449a8450430fe0
Nicolas <wiest.daessle@gmail.com>
2022-06-20 16:35:25 +0200

    Remove all collapses

--- Core/networkprocesshandler.cpp
+++ Core/networkprocesshandler.cpp
@@ -104,14 +104,15 @@ void CheckoutHttpClient::sendQueue()
 
     // Collapse request url
     QList<int> collapsed;
-    if (collapse)
-        for (int i = 0; i < reqs.size(); ++i)
-            if (reqs.at(i).url == url && (
-//                     url.path().startsWith("/addData/") ||
-//                     url.path().startsWith("/Start")  ||
-                     url.path().startsWith("/Ready") ||
-                     url.path().startsWith("/ServerDone") ) )
-                collapsed << i;
+//    if (collapse)
+//        for (int i = 0; i < reqs.size(); ++i)
+//            if (reqs.at(i).url == url && (
+////                     url.path().startsWith("/addData/") ||
+////                     url.path().startsWith("/Start")  ||
+//                     url.path().startsWith("/Ready") ||
+//                     url.path().startsWith("/ServerDone")
+//                        ) )
+//                collapsed << i;
 
     if (collapsed.size() > 0)
     {
