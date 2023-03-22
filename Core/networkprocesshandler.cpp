23c94b6f0539796232b2122ebce4ca5908b99718
Nicolas <wiest.daessle@gmail.com>
2023-03-22 11:19:28 +0100

    Fix for feature generation
    arg fix for string

--- Core/networkprocesshandler.cpp
+++ Core/networkprocesshandler.cpp
@@ -730,13 +730,30 @@ void exportBinary(QJsonObject& ds, QJsonObject& par, QCborMap& ob) // We'd like 
 
     QString plate = ds["XP"].toString(), commit = ds["CommitName"].toString();
 
+
+
+
     if (!commit.isEmpty() && par.contains(QString("SavePath")) && !par.value("SavePath").toString().isEmpty())
     {
         QString pos = par["Meta"].toArray().first().toObject()["Pos"].toString();
 
+        int t = ds["TimePos"].toInt(),
+                f = ds["FieldId"].toInt(),
+                z = ds["zPos"].toInt(),
+                ch = ds["Channel"].toInt();
+
+
+        QString ttag;
+        if (t >= 0) ttag += QString("T%1").arg(t, 3, QLatin1Char('0'));
+        if (f >= 0) ttag += QString("F%1").arg(f, 3, QLatin1Char('0'));
+        if (z >= 0) ttag += QString("Z%1").arg(z, 3, QLatin1Char('0'));
+        if (ch >= 0) ttag += QString("C%1").arg(ch, 3, QLatin1Char('0'));
+
+        ttag += par.value("Tag").toString();
+
         QString tofile = QString("%1/%2/%3_%5_%4.fth").arg(par.value("SavePath").toString(),
                                                            commit, plate,
-                                                           par.value("Tag").toString(),
+                                                           ttag,
                                                            pos);
         //        qDebug() << "Saving Generated Meta to" << tofile;
         {
