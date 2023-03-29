de5a07a86a609ad569ca82ddbced11593f570be5
cyrenaique <arno.ogier@gmail.com>
2023-03-29 16:12:42 +0200

    update phenolink

--- Core/phenolinkimage.h
+++ Core/phenolinkimage.h
@@ -1,13 +1,13 @@
 #ifndef PHENOLINKIMAGE_H
 #define PHENOLINKIMAGE_H
 
-
+#include <Dll.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
 
 namespace pl {
 
-cv::Mat imread(QString& path, int flags = cv::IMREAD_COLOR);
+cv::Mat DllCoreExport imread(QString& path, int flags = cv::IMREAD_COLOR);
 
 };
 
