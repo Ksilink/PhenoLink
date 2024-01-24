037470f8c08eede52ab6a4f61bd86805c8cf7fca
Nicolas <wiest.daessle@gmail.com>
2024-01-24 11:15:30 +0100

    Add Linux Performances counters
    To be tested on linux for proper compilation/validation

--- Core/zmq/performances.hpp
+++ Core/zmq/performances.hpp
@@ -32,7 +32,8 @@ static struct Perfs
     double total_cpu_load, proc_cpu_load;
 
 
-    Perfs()
+    Perfs(): totalPhysMem(0), physMemUsed(0), procPhysMem(0),
+        total_cpu_load(0), proc_cpu_load(0)
     {
         SYSTEM_INFO sysInfo;
         FILETIME ftime, fsys, fuser;
@@ -135,6 +136,15 @@ protected:
 
 #elif LINUX
 
+
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+
+
 // FIXME : Do it for linux also !
 static struct Perfs
 {
@@ -143,10 +153,145 @@ static struct Perfs
     double total_cpu_load, proc_cpu_load;
 
 
-    Perfs()
+    Perfs(): totalPhysMem(0), physMemUsed(0), procPhysMem(0),
+        total_cpu_load(0), proc_cpu_load(0)
     {
+        FILE* file = fopen("/proc/stat", "r");
+        fscanf(file, "cpu %llu %llu %llu %llu", &lastTotalUser, &lastTotalUserLow,
+               &lastTotalSys, &lastTotalIdle);
+        fclose(file);
+
+
+
+        struct tms timeSample;
+        char line[128];
+
+        lastCPU = times(&timeSample);
+        lastSysCPU = timeSample.tms_stime;
+        lastUserCPU = timeSample.tms_utime;
+
+        file = fopen("/proc/cpuinfo", "r");
+        numProcessors = 0;
+        while(fgets(line, 128, file) != NULL){
+            if (strncmp(line, "processor", 9) == 0) numProcessors++;
+        }
+        fclose(file);
+
 
     }
+
+
+    void refresh()
+    {
+
+        refreshCPU();
+        refresh_meminfo();
+    }
+
+
+protected:
+
+
+    unsigned long long lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
+
+    clock_t lastCPU, lastSysCPU, lastUserCPU;
+    int numProcessors;
+
+
+
+    int getMemValue(){ //Note: this value is in KB!
+        FILE* file = fopen("/proc/self/status", "r");
+        int result = -1;
+        char line[128];
+
+        while (fgets(line, 128, file) != NULL){
+            if (strncmp(line, "VmRSS:", 6) == 0){
+                result = parseLine(line);
+                break;
+            }
+        }
+        fclose(file);
+        return result;
+    }
+
+    void refresh_meminfo()
+    {
+        struct sysinfo memInfo;
+
+        sysinfo (&memInfo);
+
+
+        totalPhysMem = memInfo.totalram;
+        //Multiply in next statement to avoid int overflow on right hand side...
+        totalPhysMem *= memInfo.mem_unit;
+
+
+        physMemUsed = memInfo.totalram - memInfo.freeram;
+        //Multiply in next statement to avoid int overflow on right hand side...
+        physMemUsed *= memInfo.mem_unit;
+
+        procPhysMem = getMemValue() * 1024:
+    }
+
+    void refreshCPU()
+    {
+        double percent;
+        FILE* file;
+        unsigned long long totalUser, totalUserLow, totalSys, totalIdle, total;
+
+        file = fopen("/proc/stat", "r");
+        fscanf(file, "cpu %llu %llu %llu %llu", &totalUser, &totalUserLow,
+               &totalSys, &totalIdle);
+        fclose(file);
+
+        if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
+            totalSys < lastTotalSys || totalIdle < lastTotalIdle){
+            //Overflow detection. Just skip this value.
+            percent = -1.0;
+        }
+        else{
+            total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
+                    (totalSys - lastTotalSys);
+            percent = total;
+            total += (totalIdle - lastTotalIdle);
+            percent /= total;
+            percent *= 100;
+        }
+
+        lastTotalUser = totalUser;
+        lastTotalUserLow = totalUserLow;
+        lastTotalSys = totalSys;
+        lastTotalIdle = totalIdle;
+
+        total_cpu_load = percent;
+
+
+        struct tms timeSample;
+        clock_t now;
+
+        now = times(&timeSample);
+
+        if (now <= lastCPU || timeSample.tms_stime < lastSysCPU ||
+            timeSample.tms_utime < lastUserCPU){
+            //Overflow detection. Just skip this value.
+            percent = -1.0;
+        }
+        else{
+            percent = (timeSample.tms_stime - lastSysCPU) +
+                      (timeSample.tms_utime - lastUserCPU);
+            percent /= (now - lastCPU);
+            percent /= numProcessors;
+            percent *= 100;
+        }
+        lastCPU = now;
+        lastSysCPU = timeSample.tms_stime;
+        lastUserCPU = timeSample.tms_utime;
+
+
+        proc_cpu_load = percent;
+
+    }
+
 
 } perf_instance;
 
